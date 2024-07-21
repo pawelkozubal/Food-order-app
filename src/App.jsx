0f528e2090d20fcb50d3916d1342bb54f9ac514a
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2024-07-22 00:47:50 +0100

    Fixed the issue
    Also added more fixes to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Header from "./components/Header";
-import Meals from "./components/Meals"; 
-import { CartContextProvider } from "./components/store/CartContext";
-
+import Cart from './components/Cart.jsx';
+import Header from './components/Header.jsx';
+import Meals from './components/Meals.jsx';
+import { CartContextProvider } from './components/store/CartContext.jsx';
+import { UserProgressContextProvider } from './components/store/UserProgressContext.jsx';
 function App() {
   return (
-    <CartContextProvider>
-      <Header />  
-      <Meals /> 
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
