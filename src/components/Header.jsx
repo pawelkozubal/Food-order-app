7da2fb9300e3fbfd23cdd507ac8974c52b370b36
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2024-07-21 20:48:20 +0100

    Adding and removing items
    added an option to add and remove the items

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import logoImg from '../assets/logo.jpg'; 
+import Button from './UI/Button';
+import CartContext from './store/CartContext';
+
+export default function Header(){  
+
+    const cartCtx = useContext(CartContext); 
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item)=> {
+        return totalNumberOfItems + item.quantity; 
+    }, 0); 
+    return (
+        <header id="main-header"> 
+            <div id="title"> 
+                <img src={logoImg} alt='A restraunt'/> 
+                <h1> Pawels Diner! </h1>
+            </div> 
+            <nav>
+                <Button textOnly>Cart({totalCartItems})</Button>  
+            </nav>
+        </header>
+    );
+}
